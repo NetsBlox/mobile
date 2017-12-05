f802d6057deca5f101c3849922874595b375e163
Hamid Zare <dellydela@gmail.com>
2017-12-05 16:09:12 -0600

    WIP loading project withing ionic env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { SafePipe } from '../pipes';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -19,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
+    SafePipe,
     MyApp,
     AboutPage,
     ContactPage,
--- src/pages/editor/editor.ts
+++ src/pages/editor/editor.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Project } from '../../types';
+import common from '../../common';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -7,12 +9,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'editor.html',
 })
 export class EditorPage {
+  project:Project = common.getProjectStructure();
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    let project = this.navParams.get('project');
+    if (project) {
+      this.project = project;
+    } else {
+      console.error('project is not set');
+    }
+    console.log('got the proj', this.project);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditorPage');
   }
+
+  // gets the editor context
+  getEditorWindow() {
+    // TODO cache? 
+    let iframe: any;
+    iframe = document.querySelector('iframe#editor');
+    return iframe.contentWindow;
+  }
+
+  getWorld() {
+    return this.getEditorWindow().world;
+  }
+
+  // helper
+  applyOnEditor(fn, arg1, arg2) {
+    const context = this.getEditorWindow();
+    let args = Array.prototype.slice.call(arguments, 1);
+    return fn.apply(context, args);
+  }
+
+  // editor specific functions, this would refer to editor window
+  setFullscreen(status) {
+    this.getWorld().children[0].toggleAppMode(status);
+  }
+
 
 }
