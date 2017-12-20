1108a9481b5b3c0f44f2cfc459cdb9b2ae08f472
Hamid Zare <dellydela@gmail.com>
2017-12-20 15:35:59 -0600

    WIP building RoomManagerPage page

--- src/pages/room-manager/room-manager.ts
+++ src/pages/room-manager/room-manager.ts
@@ -8,12 +8,28 @@ import common from '../../common';
   templateUrl: 'room-manager.html',
 })
 export class RoomManagerPage {
+  roles: any;
+  room: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.roles = [{name: 'loading', occupants: [{}] ];
+    this.updateRoles();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RoomManagerPage');
+  }
+
+  getRoom() {
+    console.log('snap world', common.snap.world);
+    return common.snap.world.children[0].room;
+  }
+
+  updateRoles() {
+    let room = this.getRoom();
+    console.log('room', room);
+    console.log('RoomMorph', Object.keys(common.snap.RoomMorph.prototype));
+    this.roles = room.getRoles();
   }
 
 }
