9d632288069d02a609cce0e121c890fcc3a77e8d
Hamid Zare <dellydela@gmail.com>
2018-01-26 15:20:14 -0600

    added required android premissions to androidmanifest

--- src/netsblox-overrides/main.js
+++ src/netsblox-overrides/main.js
@@ -1,1 +1,22 @@
-TESTING='DONE';
+// overriding netsblox/snap features for mobile use
+console.log('overriding netsblox for mobile use');
+
+// override helper
+let branchFn = (originalFn, condition, newFn) => {
+  let self = this;
+  return function() {
+    if (condition()) {
+      return newFn.apply(self, arguments);
+    } // else
+    return originalFn.apply(self, arguments);
+  };
+};
+
+
+// geolocation
+
+NetsProcess.prototype.getLocation = () => {
+  return {
+    latitude: 36, longitude: 86
+  };
+};
