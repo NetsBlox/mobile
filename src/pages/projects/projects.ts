320bad906ed84cce2b8ccb977e75dd31ca26c670
Hamid Zare <dellydela@gmail.com>
2018-02-21 12:58:56 -0600

    made caching optional on loadUserProjects

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -16,7 +16,6 @@ export class ProjectsPage {
   publicProjects: Project[];
   exampleProjects:  Project[] = [];
   category: string = 'examples'; // default category to show
-  cache:Cache = common.cache;
   state:State = common.state; // TODO authentication should be handled in form of a middleware
 
   // TODO use native cordova http module for fetching
@@ -31,7 +30,7 @@ export class ProjectsPage {
 
   ionViewWillEnter() {
     console.log('ionViewWillEnter ProjectsPage');
-    if (!this.cache.projects) this.loadUserProjects();
+    if (!common.cache.projects) this.loadUserProjects();
   }
 
   itemSelected(project) {
@@ -69,11 +68,12 @@ export class ProjectsPage {
   }
 
   // loads the projects from cache or request the server for projects
-  loadUserProjects() {
+  // cache flag dictates whether to use the cache (if available) or not
+  loadUserProjects(cache=true) {
     console.log('loading projects');
-    if (this.cache.projects) {
+    if (cache && common.cache.projects) {
       console.log('loading projects from cache');
-      this.projects = [...this.cache.projects];
+      this.projects = [...common.cache.projects];
       return;
     }
     let url = common.SERVER_ADDRESS + '/api/getProjectList?format=json';
@@ -99,7 +99,7 @@ export class ProjectsPage {
           };
         });
         projects.sort((a,b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1)
-        this.cache.projects = projects;  
+        common.cache.projects = projects;
         this.projects = [...projects];
         return projects
       })
