3aa3d6f17ffee612d1f232154659923dba566b85
Hamid Zare <dellydela@gmail.com>
2018-04-16 12:24:18 -0500

    added simple releasenotes and version checking

--- bin/publish-android.sh
+++ bin/publish-android.sh
@@ -2,7 +2,7 @@
 
 keystore=$1
 alias=$2
-version=$3
+version=$(cat config.xml| grep widget | grep -Eo 'version="(.{2,6})"' | grep -Eo '".*"' | sed 's/"//g')
 app_name=netsblox
 apk_dir=platforms/android/build/outputs/apk
 final_name="$app_name-release-$version.apk"
@@ -17,10 +17,25 @@ function safe_deletion() {
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
     rm -i $apk_dir/$final_name
+  else
+    exit 1
   fi
 }
 
+function check_version() {
+  echo "You are about to publish version $version (major, minor, patch)"
+  read -p "Are you sure? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    echo ok
+  else
+    echo bump the version in config.xml
+    exit 1
+  fi
+}
 
+check_version
 [ -f $apk_dir/$final_name ] && safe_deletion
 
 # jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $keystore $apk_dir/$unsigned_name $alias
@@ -28,6 +43,13 @@ function safe_deletion() {
 # zipalign -v 4 $apk_dir/$unsigned_name $apk_dir/$final_name
 
 ionic cordova build android --prod --release -- -- --keystore=$keystore --alias=$alias
-
 mv $apk_dir/android-release.apk $apk_dir/$final_name
+
+# keep notes
+notes_file=$apk_dir/$version-notes.txt
+echo "#############=====###########" > $notes_file
+git log > $notes_file
+echo "#############" > $notes_file
+git status > $notes_file
+
 echo published $app_name $version to $apk_dir/$final_name
