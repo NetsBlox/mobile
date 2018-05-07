d2d9a5be228686f992ea46be729bd7376c6f1fc5
Hamid Zare <dellydela@gmail.com>
2018-05-07 12:29:07 -0500

    show current server address by long pressing logo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 import { State } from '../../types';
+import { Utils } from '../../utils';
 import common from '../../common';
 
 @Component({
@@ -12,8 +13,15 @@ export class HomePage {
   LoginPage: any;
   state:State = common.state;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public utils: Utils
+  ) {
     this.LoginPage = LoginPage;
+  }
+
+  showServer() {
+    this.utils.presentToast(common.SERVER_ADDRESS);
   }
 
 }
